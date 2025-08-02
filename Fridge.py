9f7ac436cf10942bdb0794ace102f3669f507d32
samnfbgvn <samuel.samanek@gmail.com>
2025-08-02 15:38:50 +0200

    spliting into files

--- Fridge.py
+++ Fridge.py
@@ -1,124 +1,61 @@
+import requests
+import json
 
-groceries = {
+class Fridge:
+    def __init__(self, groceries):
+        self.groceries = groceries
+
+    def add_item(self, name, count):
+        name = name.title() 
+        if name in self.groceries:
+            self.groceries[name] += count
+        else:
+            self.groceries[name] = count
+
+
+    def log_groceries(self):    
+        for name, quantity  in self.groceries.items():
+            print(f"- {name}: {quantity } ks")
+
+    
+    def rename_item(self, old_name, new_name):
+        old_name = old_name.title()
+        new_name = new_name.title()
+
+        if old_name in self.groceries:
+            if new_name in self.groceries:
+                print(f"Item '{new_name}' already exists.")
+            else:
+                self.groceries[new_name] = self.groceries.pop(old_name)
+                print(f"Renamed '{old_name}' to '{new_name}'")
+          
+        else:
+            print(f"{old_name} does not exist in your fridge.")
+
+    def remove_item(self, name, count):
+        name = name.title()
+        if name in self.groceries:
+            if self.groceries[name] >= count:
+                self.groceries[name] -= count
+                if self.groceries[name] == 0:
+                    del self.groceries[name]
+            else:
+                print(f"You only have {self.groceries[name]} of {name} you took too much.")
+        else:
+            print(f"{name} is not in your fridge.")
+
+    
+
+                
+
+
+
+if __name__ == "__main__":
+    groceries = {
     "Salat": 2,
     "Tomato": 4,
     "Peach": 7
-}
-
-
-
-def main():
-    answer = input("Do you wanna now what you have in fridge? Yes/No: ")
-    if answer.lower() == "yes":
-        print("Your storage:\n")
-        log_groceries()
-        print("\n")
-    else:
-        print("Alright, closing the fridge")
-    
-    handle_menu()
+    }
+    program = Fridge(groceries)
+    program.main()
   
-   
-def handle_menu():
-    while True:
-        answer = get_user_input("Do you want something to 'Add' = 1, 'Delete' = 2, 'Rename' = 3, 'Show Items' = 4, 'Close' = 5 -> ")
-        if answer == "1":
-            name, count = get_name_and_quantity_input()
-            add_grocery_item(name.title(), count)
-            log_groceries()
-
-        elif answer == "2":
-            name, count = get_name_and_quantity_input()
-            substract_grocery_item(name.title(), count)
-            log_groceries()
-            
-        elif answer == "3":
-            name = get_user_input("Which item do you want to rename? ").title()
-            rename(name)
-            log_groceries()
-        elif answer == "4":
-            print("Your items in fridge: ")
-            log_groceries()
-        
-        elif answer == "5":
-            print(" Closing the fridge.")
-            return
-        
-        else:
-            print(" Invalid input – please type a number between 1 and 5.")
-
-       
-
-            
-
-def get_user_input(message):
-    response = input(message)
-    return response
-
-
-def get_name_and_quantity_input():
-    while True:
-        name = get_user_input("Enter the name of the grocery: ")
-        if not is_input_string(name):
-            continue
-        break
-
-    while True: 
-        count = get_user_input("Enter the quantity: ")
-        if not is_input_positive_digit(count):
-            continue
-        break
-
-    return name.lower(), int(count)
-
-def add_grocery_item(name, count):
-    if name in groceries:
-        groceries[name] += count
-        return count
-    else:
-        groceries[name] = count
-
-def substract_grocery_item(name, count):
-    if name in groceries:
-        if groceries[name] >= count:
-            groceries[name] -= count
-            if groceries[name] == 0:
-                del groceries[name]
-        else:
-            print("you took too much, you dont have that many")
-    else:
-        print("this item is not in your fridge. ")
-
-def rename(name):
-    if name in groceries:
-        response = get_user_input("Enter new name: ")
-        new_name = response.title()
-        groceries[new_name] = groceries[name]
-        del groceries[name]
-        print(f"Renamed '{name}' to '{new_name}'")
-    else:
-        print("You cannot rename some that doesnt exist")
-
-
-
-def log_groceries():    
-    for name, quantity  in groceries.items():
-        print(f"- {name}: {quantity } ks")
-
-
-def is_input_string(text):
-    if text.isalpha():
-        return True
-    else:
-        print("Your log needs to have only letters")
-        return False
-
-def is_input_positive_digit(text):
-    if text.isdigit() and int(text) > 0:
-        return True
-    else:
-        print("Your log may have only positive number")
-        return False
-    
-
-main()
